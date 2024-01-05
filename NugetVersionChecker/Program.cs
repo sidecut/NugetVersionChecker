ae3d36319a7da55c3a766ec00f3b2220484ab4c3
Jim Raden <james.raden@gmail.com>
2024-01-05 16:02:26 -0500

    Convert to builder style in order to inject logging

--- NugetVersionChecker/Program.cs
+++ NugetVersionChecker/Program.cs
@@ -1,8 +1,16 @@
 ﻿using System.Xml.Linq;
 using Cocona;
+using Microsoft.Extensions.Logging;
 using NugetVersionChecker;
 
-CoconaApp.Run(([Option('p', Description = "Project file name, e.g. /path/to/project/myproject.csproj")][PathExists] string project) =>
+var builder = CoconaApp.CreateBuilder();
+builder.Logging.AddDebug();
+
+var app = builder.Build();
+app.AddCommand((ILogger<Program> logger, [Option('p', Description = "Project file name, e.g. /path/to/project/myproject.csproj")][PathExists] string project) =>
 {
+    logger.LogInformation("Analyzing project file {project}:", project);
     var projectFile = XDocument.Load(project);
 });
+
+app.Run();
