8cf56d9c3797c83d38342817752faddd7e39d628
Jim Raden <james.raden@gmail.com>
2024-01-05 15:45:09 -0500

    POC of Cocona CLI helper

--- NugetVersionChecker/Program.cs
+++ NugetVersionChecker/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.ComponentModel;
+using System.Xml;
+using System.Xml.Linq;
+using System.Xml.Serialization;
+using Cocona;
+
+CoconaApp.Run(([Option('p', Description = "Project file name, e.g. /path/to/project/myproject.csproj")] string project) =>
+{
+    var projectFile = XDocument.Load(project);
+});
