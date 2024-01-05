4500a5dbf39e967a476fa947aa0b136e1b90510b
Jim Raden <james.raden@gmail.com>
2024-01-05 15:55:46 -0500

    Add PathExists validation checker

---
+++ NugetVersionChecker/PathExists.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NugetVersionChecker;
+
+class PathExistsAttribute : ValidationAttribute
+{
+    protected override ValidationResult IsValid(object? value, ValidationContext validationContext)
+    {
+        if (value is string path && File.Exists(path))
+        {
+#pragma warning disable CS8603 // Possible null reference return.
+            return ValidationResult.Success;
+#pragma warning restore CS8603 // Possible null reference return.
+        }
+        return new ValidationResult($"The path '{value}' is not found.");
+    }
+}
--- NugetVersionChecker/Program.cs
+++ NugetVersionChecker/Program.cs
@@ -1,10 +1,8 @@
-﻿using System.ComponentModel;
-using System.Xml;
-using System.Xml.Linq;
-using System.Xml.Serialization;
+﻿using System.Xml.Linq;
 using Cocona;
+using NugetVersionChecker;
 
-CoconaApp.Run(([Option('p', Description = "Project file name, e.g. /path/to/project/myproject.csproj")] string project) =>
+CoconaApp.Run(([Option('p', Description = "Project file name, e.g. /path/to/project/myproject.csproj")][PathExists] string project) =>
 {
     var projectFile = XDocument.Load(project);
 });
